b57c8e340fe33b8fea292980006114cd4f979161
Darryl Abbate <rootbeersoup@gmail.com>
2020-06-19 00:25:39 -0500

    Minor code org

--- src/disas.c
+++ src/disas.c
@@ -2,69 +2,45 @@
 
 #include "disas.h"
 
+static const char *tokenstr[] = {
+    [TK_AND] =
+    "&&", "::", "--", "==", ">=", "++", "<=", "!=", "||", "**",
+    "<<", ">>", "+=", "&=", "/=", "%=", "*=", "|=", "-=", "^=",
+    "::=", "**=", "<<=", ">>=",
+    "break", "else", "exit", "fn", "for", "if", "local", "print",
+    "return", "while"
+};
+
 #define OPTR_STR "<Operator, %s >"
 #define KW_STR   "<Keyword, %s>"
 
 static void tk2str(token_t *tk, char *s) {
-    switch(tk->type) {
-    case '+': case '-': case '*': case '/': case '%':
-    case '!': case '&': case '|': case '^': case '<':
-    case '>': case '=': case '?': case ':': case '#':
-    case '(': case ')': case '[': case ']': case '{':
-    case '}': case '~': case ';': case ',':
+    if (tk->type < TK_AND)
         sprintf(s, "<Operator, %c >", tk->type);
-        break;
-    case TK_AND:        sprintf(s, OPTR_STR, "&&");  break;
-    case TK_CAT:        sprintf(s, OPTR_STR, "::");  break;
-    case TK_DEC:        sprintf(s, OPTR_STR, "--");  break;
-    case TK_EQ:         sprintf(s, OPTR_STR, "==");  break;
-    case TK_GE:         sprintf(s, OPTR_STR, ">=");  break;
-    case TK_INC:        sprintf(s, OPTR_STR, "++");  break;
-    case TK_LE:         sprintf(s, OPTR_STR, "<=");  break;
-    case TK_NE:         sprintf(s, OPTR_STR, "!=");  break;
-    case TK_OR:         sprintf(s, OPTR_STR, "||");  break;
-    case TK_POW:        sprintf(s, OPTR_STR, "**");  break;
-    case TK_SHL:        sprintf(s, OPTR_STR, "<<");  break;
-    case TK_SHR:        sprintf(s, OPTR_STR, ">>");  break;
-    case TK_ADD_ASSIGN: sprintf(s, OPTR_STR, "+=");  break;
-    case TK_AND_ASSIGN: sprintf(s, OPTR_STR, "&=");  break;
-    case TK_DIV_ASSIGN: sprintf(s, OPTR_STR, "/=");  break;
-    case TK_MOD_ASSIGN: sprintf(s, OPTR_STR, "%=");  break;
-    case TK_MUL_ASSIGN: sprintf(s, OPTR_STR, "*=");  break;
-    case TK_OR_ASSIGN:  sprintf(s, OPTR_STR, "|=");  break;
-    case TK_SUB_ASSIGN: sprintf(s, OPTR_STR, "-=");  break;
-    case TK_XOR_ASSIGN: sprintf(s, OPTR_STR, "^=");  break;
-    case TK_CAT_ASSIGN: sprintf(s, OPTR_STR, "::="); break;
-    case TK_POW_ASSIGN: sprintf(s, OPTR_STR, "**="); break;
-    case TK_SHL_ASSIGN: sprintf(s, OPTR_STR, "<<="); break;
-    case TK_SHR_ASSIGN: sprintf(s, OPTR_STR, ">>="); break;
-
-    case TK_BREAK:  sprintf(s, KW_STR, "break");  break;
-    case TK_ELSE:   sprintf(s, KW_STR, "else");   break;
-    case TK_EXIT:   sprintf(s, KW_STR, "exit");   break;
-    case TK_FN:     sprintf(s, KW_STR, "fn");     break;
-    case TK_FOR:    sprintf(s, KW_STR, "for");    break;
-    case TK_IF:     sprintf(s, KW_STR, "if");     break;
-    case TK_LOCAL:  sprintf(s, KW_STR, "local");  break;
-    case TK_PRINT:  sprintf(s, KW_STR, "print");  break;
-    case TK_RETURN: sprintf(s, KW_STR, "return"); break;
-    case TK_WHILE:  sprintf(s, KW_STR, "while");  break;
-
-    case TK_FLT:
-        sprintf(s, "<Float, %f (0x%a)>", tk->lexeme.f, tk->lexeme.f);
-        break;
-    case TK_INT:
-        sprintf(s, "<Int, %lld (0x%llx)>", tk->lexeme.i, tk->lexeme.i);
-        break;
-    case TK_STR:
-        sprintf(s, "<String (%zu), %s>", tk->lexeme.s->l, tk->lexeme.s->str);
-        break;
-    case TK_ID:
-        sprintf(s, "<Identifier, %s>", tk->lexeme.s->str);
-        break;
-    default:
-        sprintf(s, "Unidentified token");
-        break;
+    else if (tk->type < TK_BREAK)
+        sprintf(s, OPTR_STR, tokenstr[tk->type]);
+    else if (tk->type < TK_FLT)
+        sprintf(s, KW_STR, tokenstr[tk->type]);
+    else {
+        switch(tk->type) {
+        case TK_FLT:
+            sprintf(s, "<Float, %f (0x%a)>", tk->lexeme.f, tk->lexeme.f);
+            break;
+        case TK_INT:
+            sprintf(s, "<Int, %lld (0x%llx)>", tk->lexeme.i, tk->lexeme.i);
+            break;
+        case TK_STR:
+            sprintf(s, "<String (%zu, 0x%llx), %s>", tk->lexeme.s->l,
+                                                     tk->lexeme.s->hash,
+                                                     tk->lexeme.s->str);
+            break;
+        case TK_ID:
+            sprintf(s, "<Identifier, %s>", tk->lexeme.s->str);
+            break;
+        default:
+            sprintf(s, "Unidentified token");
+            break;
+        }
     }
 }
 
@@ -74,9 +50,9 @@ static void tk2str(token_t *tk, char *s) {
 void print_tk_stream(const char *src) {
     lexer_t x;
     x_init(&x, src);
-    while (!x_next(&x)) {
+    do {
         char s[1024];
         tk2str(&x.tk, s);
-        printf("%s\n", s);
-    }
+        puts(s);
+    } while (!x_next(&x));
 }
