ae1341a5a83d9ff12d870fc912a40d8b087a9db5
Darryl Abbate <rootbeersoup@gmail.com>
2020-06-21 16:57:52 -0500

    Parse parenthesized exprs

--- src/parse.c
+++ src/parse.c
@@ -3,16 +3,50 @@
 #include "parse.h"
 #include "types.h"
 
-#define adv(y) x_adv(y->x)
+#define adv(y)  x_adv(y->x)
 #define push(b) c_push(y->c, b)
 
-static void check(lexer_t *x, int tk) {
+static void err(parser_t *y, const char *msg) {
+    fprintf(stderr, "line %d: %s\n", y->x->ln, msg);
+    exit(1);
 }
 
-static int  expr(parser_t *y, int rbp);
-static void nud(parser_t *y);
-static void led(parser_t *y, int tk);
-static int  lbp(int tk);
+static void check(parser_t *y, int tk) {
+    if (y->x->tk.type != tk) {
+        char msg[80];
+        sprintf(msg, "Missing '%c'", tk);
+        err(y, msg);
+    }
+}
+
+static int expr(parser_t *y, int rbp);
+
+// LBP for non-unary operators
+static int lbp(int tk) {
+    switch (tk) {
+    case TK_POW:                  return 14;
+    case '*': case '/': case '%': return 12;
+    case '+': case '-':           return 11;
+    case TK_CAT:                  return 10;
+    case TK_SHL: case TK_SHR:     return 10;
+    case '>': case '<':      
+    case TK_GE: case TK_LE:       return 9;
+    case TK_EQ: case TK_NE:       return 8;
+    case '&':                     return 7;
+    case '^':                     return 6;
+    case '|':                     return 5;
+    case TK_AND:                  return 4;
+    case TK_OR:                   return 3;
+    case '?': case ':':           return 2;
+    case '=':
+    case TK_ADD_ASSIGN: case TK_AND_ASSIGN: case TK_DIV_ASSIGN:
+    case TK_MOD_ASSIGN: case TK_MUL_ASSIGN: case TK_OR_ASSIGN:
+    case TK_SUB_ASSIGN: case TK_XOR_ASSIGN: case TK_CAT_ASSIGN:
+    case TK_POW_ASSIGN: case TK_SHL_ASSIGN: case TK_SHR_ASSIGN:
+        return 1;
+    default: return 0;
+    }
+}
 
 static int uop(int tk) {
     switch (tk) {
@@ -75,9 +109,10 @@ static void nud(parser_t *y) {
     }
     switch (tk) {
     case '(':
+        adv(y);
         expr(y, 0);
-        // Check for closing ')'?
-        break;
+        check(y, ')');
+        return;
     case TK_DEC: // Pre-decrement
     case TK_INC: // Pre-increment
         break;
@@ -130,6 +165,10 @@ static void led(parser_t *y, int tk) {
     }
     switch (tk) {
     case TK_NE:
+        expr(y, lbp(tk));
+        push(OP_EQ);
+        push(OP_NEG);
+        return;
     case '?':
     case ':':
     case '(': // Function call
@@ -150,39 +189,12 @@ static void led(parser_t *y, int tk) {
     }
 }
 
-// LBP for non-unary operators
-static int lbp(int tk) {
-    switch (tk) {
-    case TK_POW:                  return 14;
-    case '*': case '/': case '%': return 12;
-    case '+': case '-':           return 11;
-    case TK_CAT:                  return 10;
-    case TK_SHL: case TK_SHR:     return 10;
-    case '>': case '<':      
-    case TK_GE: case TK_LE:       return 9;
-    case TK_EQ: case TK_NE:       return 8;
-    case '&':                     return 7;
-    case '^':                     return 6;
-    case '|':                     return 5;
-    case TK_AND:                  return 4;
-    case TK_OR:                   return 3;
-    case '?': case ':':           return 2;
-    case '=':
-    case TK_ADD_ASSIGN: case TK_AND_ASSIGN: case TK_DIV_ASSIGN:
-    case TK_MOD_ASSIGN: case TK_MUL_ASSIGN: case TK_OR_ASSIGN:
-    case TK_SUB_ASSIGN: case TK_XOR_ASSIGN: case TK_CAT_ASSIGN:
-    case TK_POW_ASSIGN: case TK_SHL_ASSIGN: case TK_SHR_ASSIGN:
-        return 1;
-    default: return 0;
-    }
-}
-
 static int expr(parser_t *y, int rbp) {
     nud(y);
     int op = y->x->tk.type;
 
     // Hack for unary
-    if (!(lbop(op) || rbop(op))) {
+    if (!lbp(op)) {
         adv(y);
         op = y->x->tk.type;
     }
@@ -193,12 +205,13 @@ static int expr(parser_t *y, int rbp) {
     return 0;
 }
 
+// Standalone expressions
 // Evaluates whether a standalone expression had any side effects. If
 // a standalone expression is given without any assignment, its result
 // will be printed.
 static void expr_stmt(parser_t *y) {
     expr(y, 0);
-    if (y->a)
+    if (!y->a)
         push(OP_PRINT);
     y->a = 0;
 }
@@ -213,24 +226,24 @@ static void if_stmt(parser_t *y) {
 }
 
 static void ret_stmt(parser_t *y) {
+    expr(y, 0);
+    push(OP_RET);
 }
 
 static void while_stmt(parser_t *y) {
 }
 
 static void stmt(parser_t *y) {
-    int tt = y->x->tk.type;
-    switch(tt) {
-    // case ';': x_adv(y->x); break;
-    // case TK_FN: x_adv(y->x); fn_def(y);
-    case TK_FOR:
-    case TK_IF:
-    case TK_LOCAL:
-    case TK_RETURN:
-    case TK_WHILE:
-    default: expr_stmt(y); break;
+    switch(y->x->tk.type) {
+    case ';':       adv(y);                break;
+    case TK_FN:     adv(y); fn_def(y);     break;
+    case TK_FOR:    adv(y); for_stmt(y);   break;
+    case TK_IF:     adv(y); if_stmt(y);    break;
+    case TK_LOCAL: break;
+    case TK_RETURN: adv(y); ret_stmt(y);   break;
+    case TK_WHILE:  adv(y); while_stmt(y); break;
+    default: expr_stmt(y);                 break;
     }
-    push(OP_RET0);
 }
 
 static void stmt_list(parser_t *y) {
@@ -243,7 +256,6 @@ int y_compile(const char *src, chunk_t *c) {
     y.x = &x;
     y.c = c;
     stmt(&y);
-    // c_init(y.c);
     return 0;
 }
 
