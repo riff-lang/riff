004bed94d774a62f0eaedfcde1ad38769f8f9ec1
Darryl Abbate <rootbeersoup@gmail.com>
2020-07-07 16:24:02 -0500

    Pretty sure this works

--- src/table.c
+++ src/table.c
@@ -1,7 +1,5 @@
 #include "mem.h"
 #include "table.h"
-#include <stdio.h>
-#include <stdint.h>
 
 #define LOAD_FACTOR 0.7
 
@@ -26,39 +24,64 @@ static entry_t *new_entry(str_t *k, val_t *v) {
     return e;
 }
 
-// Return suitable index in the given hash table for a given key
-static int t_index(table_t *t, uint32_t key) {
-    int i = key & (t->cap - 1);
-    printf("%d\n", i);
-    while (t->e[i] != NULL && t->e[i]->key->hash != key) {
-        i = (i + 1) & (t->cap - 1);
-        printf("%d\n", t->cap);
+// Given a string's hash, return index of the element exists in the
+// table, or an index of a suitble empty slot
+static int t_index(entry_t **e, int cap, uint32_t key) {
+    int i = key & (cap - 1);
+    while (e[i] && e[i]->key->hash != key) {
+        i = (i << 1) & (cap - 1); // Quadratic probing
     }
     return i;
 }
 
+static int t_exists(table_t *t, str_t *k) {
+    int i = t_index(t->e, t->cap, k->hash);
+    return t->e[i] && t->e[i]->key->hash == k->hash;
+}
+
 val_t *t_lookup(table_t *t, str_t *k) {
-    int i = t_index(t, k->hash);
-    if (t->e[i] == NULL)
+    int i = t_index(t->e, t->cap, k->hash);
+    if (!t->e[i])
         return v_newint(0);
     else
         return t->e[i]->val;
 }
 
 void t_insert(table_t *t, str_t *k, val_t *v) {
-    eval_resize_lf(t->e, t->n, t->cap, LOAD_FACTOR);
-    // if ((t->cap * TBL_MAX_LOAD) <= t->n + 1) {
-    //     t->cap = increase_cap(t->cap);
-    //     t->e   = realloc(t->e, sizeof(entry_t *) * t->cap);
-    // }
-    int i = t_index(t, k->hash);
+    // Evaluate hash table size
+    if ((t->cap * LOAD_FACTOR) <= t->n + 1) {
+        int nc = t->cap < 8 ? 8 : t->cap * 2;
+        entry_t **ne = calloc(nc, sizeof(entry_t *));
+        int nn = 0;
+        int j;
+        if (t->n) {
+            for (int i = 0; t->n || i < t->cap; i++) {
+                if (!t->e[i]) {
+                    continue;
+                } else {
+                    j = t_index(ne, nc, t->e[i]->key->hash);
+                    ne[j] = t->e[i];
+                    nn++;
+                    t->n--;
+                }
+            }
+        }
+        free(t->e);
+        t->e   = ne;
+        t->n   = nn;
+        t->cap = nc;
+    }
+    int i = t_index(t->e, t->cap, k->hash);
+    if (!t_exists(t, k)) {
+        t->n++;
+        free(t->e[i]);
+    }
     t->e[i] = new_entry(k, v);
-
 }
 
+// TODO
 void t_delete(table_t *t, str_t *k) {
     t->n--;
 }
 
 #undef LOAD_FACTOR
-#undef probe
--- src/test.c
+++ src/test.c
@@ -3,13 +3,55 @@
 #include "str.h"
 #include "table.h"
 
+static const char *keys[] = {
+    "sds0",
+    "sff1",
+    "lll2",
+    "hgf3",
+    "kfs4",
+    "0df5",
+    "ghn6",
+    "qpx7",
+    "ipc8",
+    "dpa9"
+    // "key0",
+    // "key1",
+    // "key2",
+    // "key3",
+    // "key4",
+    // "key5",
+    // "key6",
+    // "key7",
+    // "key8",
+    // "key9"
+};
+
+static void t_print(table_t *t, int c) {
+    for (int i = 0; i < c; i++) {
+        if (!t->e[i])
+            printf("[%d] (empty)\n", i);
+        else
+            printf("[%d] Key: %s, Value: %lld\n", 
+                    i,
+                    t->e[i]->key->str,
+                    t->e[i]->val->u.i);
+    }
+}
+
 int main(void) {
-    char *str = "hello";
-    str_t *s = s_newstr(str, 5);
     table_t t;
-    val_t *v = v_newint(24);
     t_init(&t);
-    t_insert(&t, s, v);
-    printf("%lld\n", t_lookup(&t, s)->u.i);
+    for (int j = 0; j < 10; j++) {
+        t_insert(&t, s_newstr(keys[j], 4), v_newint(j));
+        printf("Iteration %d:\n", j);
+        t_print(&t, t.cap);
+        printf("\n");
+    }
+    printf("Final table:\n");
+    for (int j = 0; j < 10; j++) {
+        printf("Key: %s, Value: %lld\n",
+                keys[j],
+                t_lookup(&t, s_newstr(keys[j], 4))->u.i);
+    }
     return 0;
 }
