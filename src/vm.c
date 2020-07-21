6529fc987331e5093a998f7b7f83ddfd6d8991a4
Darryl Abbate <rootbeersoup@gmail.com>
2020-07-21 14:26:33 -0500

    Progress

--- src/vm.c
+++ src/vm.c
@@ -18,7 +18,6 @@ static val_t *deref(val_t *v) {
 
 // Return logical result of value
 static int test(val_t *v) {
-    v = deref(v);
     switch (v->type) {
     case TYPE_INT: return !!(v->u.i);
     case TYPE_FLT: return !!(v->u.f);
@@ -36,86 +35,86 @@ static int test(val_t *v) {
 #define fltval(x) (IS_FLT(x) ? x->u.f : \
                    IS_INT(x) ? (flt_t) x->u.i : 0)
 
-#define int_arith(p,l,r,op) \
-    assign_int(p, (intval(l) op intval(r)));
+#define int_arith(l,r,op) \
+    assign_int(l, (intval(l) op intval(r)));
 
-#define flt_arith(p,l,r,op) \
-    assign_flt(p, (numval(l) op numval(r)));
+#define flt_arith(l,r,op) \
+    assign_flt(l, (numval(l) op numval(r)));
 
-#define num_arith(p,l,r,op) \
+#define num_arith(l,r,op) \
     if (IS_FLT(l) || IS_FLT(r)) { \
-        flt_arith(p,l,r,op); \
+        flt_arith(l,r,op); \
     } else { \
-        int_arith(p,l,r,op); \
+        int_arith(l,r,op); \
     }
 
-void z_add(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,+); }
-void z_sub(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,-); }
-void z_mul(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,*); }
-void z_div(val_t *p, val_t *l, val_t *r) { flt_arith(p,l,r,/); }
+void z_add(val_t *l, val_t *r) { num_arith(l,r,+); }
+void z_sub(val_t *l, val_t *r) { num_arith(l,r,-); }
+void z_mul(val_t *l, val_t *r) { num_arith(l,r,*); }
+void z_div(val_t *l, val_t *r) { flt_arith(l,r,/); }
 
 // TODO Make sure this works as intended
 // TODO Error handling, e.g. RHS = 0
-void z_mod(val_t *p, val_t *l, val_t *r) {
+void z_mod(val_t *l, val_t *r) {
     flt_t res = fmod(numval(l), numval(r));
-    assign_flt(p, res < 0 ? res + numval(r) : res);
+    assign_flt(l, res < 0 ? res + numval(r) : res);
 }
 
-void z_pow(val_t *p, val_t *l, val_t *r) {
-    assign_flt(p, pow(fltval(l), fltval(r)));
+void z_pow(val_t *l, val_t *r) {
+    assign_flt(l, pow(fltval(l), fltval(r)));
 }
 
-void z_and(val_t *p, val_t *l, val_t *r) { int_arith(p,l,r,&);  }
-void z_or(val_t *p, val_t *l, val_t *r)  { int_arith(p,l,r,|);  }
-void z_xor(val_t *p, val_t *l, val_t *r) { int_arith(p,l,r,^);  }
-void z_shl(val_t *p, val_t *l, val_t *r) { int_arith(p,l,r,<<); }
-void z_shr(val_t *p, val_t *l, val_t *r) { int_arith(p,l,r,>>); }
+void z_and(val_t *l, val_t *r) { int_arith(l,r,&);  }
+void z_or(val_t *l, val_t *r)  { int_arith(l,r,|);  }
+void z_xor(val_t *l, val_t *r) { int_arith(l,r,^);  }
+void z_shl(val_t *l, val_t *r) { int_arith(l,r,<<); }
+void z_shr(val_t *l, val_t *r) { int_arith(l,r,>>); }
 
 // TODO
-void z_num(val_t *p, val_t *v) {
+void z_num(val_t *v) {
     switch (v->type) {
     case TYPE_INT:
-        assign_int(p, intval(v));
+        assign_int(v, intval(v));
         break;
     case TYPE_FLT:
-        assign_flt(p, fltval(v));
+        assign_flt(v, fltval(v));
         break;
     default:
-        assign_int(p, 0);
+        assign_int(v, 0);
         break;
     }
 }
 
-void z_neg(val_t *p, val_t *v) {
+void z_neg(val_t *v) {
     switch (v->type) {
     case TYPE_INT:
-        assign_int(p, -intval(v));
+        assign_int(v, -intval(v));
         break;
     case TYPE_FLT:
-        assign_flt(p, -fltval(v));
+        assign_flt(v, -fltval(v));
         break;
     default:
-        assign_int(p, -1); // TODO type coercion
+        assign_int(v, -1); // TODO type coercion
         break;
     }
 }
 
-void z_not(val_t *p, val_t *v) {
-    assign_int(p, ~intval(v));
+void z_not(val_t *v) {
+    assign_int(v, ~intval(v));
 }
 
-void z_eq(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,==); }
-void z_ne(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,!=); }
-void z_gt(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,>);  }
-void z_ge(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,>=); }
-void z_lt(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,<);  }
-void z_le(val_t *p, val_t *l, val_t *r) { num_arith(p,l,r,<=); }
+void z_eq(val_t *l, val_t *r) { num_arith(l,r,==); }
+void z_ne(val_t *l, val_t *r) { num_arith(l,r,!=); }
+void z_gt(val_t *l, val_t *r) { num_arith(l,r,>);  }
+void z_ge(val_t *l, val_t *r) { num_arith(l,r,>=); }
+void z_lt(val_t *l, val_t *r) { num_arith(l,r,<);  }
+void z_le(val_t *l, val_t *r) { num_arith(l,r,<=); }
 
-void z_lnot(val_t *p, val_t *v) {
-    assign_int(p, !numval(v));
+void z_lnot(val_t *v) {
+    assign_int(v, !numval(v));
 }
 
-void z_len(val_t *p, val_t *v) {
+void z_len(val_t *v) {
     int_t l = 0;
     switch (v->type) {
     case TYPE_INT: l = s_int2str(v->u.i)->l; break;
@@ -125,14 +124,14 @@ void z_len(val_t *p, val_t *v) {
     case TYPE_FN:  // TODO
     default: break;
     }
-    assign_int(p, l);
+    assign_int(v, l);
 }
 
-void z_test(val_t *p, val_t *v) {
-    assign_int(p, test(v));
+void z_test(val_t *v) {
+    assign_int(v, test(v));
 }
 
-void z_cat(val_t *p, val_t *l, val_t *r) {
+void z_cat(val_t *l, val_t *r) {
     switch (l->type) {
     case TYPE_VOID: l->u.s = s_newstr(NULL, 0, 0); break;
     case TYPE_INT:  l->u.s = s_int2str(intval(l)); break;
@@ -147,26 +146,26 @@ void z_cat(val_t *p, val_t *l, val_t *r) {
     default: break;
     }
 
-    assign_str(p, s_concat(l->u.s, r->u.s, 0));
+    assign_str(l, s_concat(l->u.s, r->u.s, 0));
 }
 
 // Potentially very slow; allocates 2 new string objects for every int
 // or float LHS
 // TODO Index out of bounds handling, e.g. RHS > length of LHS
-void z_idx(val_t *p, val_t *l, val_t *r) {
+void z_idx(val_t *l, val_t *r) {
     switch (l->type) {
     case TYPE_INT:
-        assign_str(p, s_newstr(&s_int2str(l->u.i)->str[intval(r)], 1, 0));
+        assign_str(l, s_newstr(&s_int2str(l->u.i)->str[intval(r)], 1, 0));
         break;
     case TYPE_FLT:
-        assign_str(p, s_newstr(&s_flt2str(l->u.f)->str[intval(r)], 1, 0));
+        assign_str(l, s_newstr(&s_flt2str(l->u.f)->str[intval(r)], 1, 0));
         break;
     case TYPE_STR:
-        assign_str(p, s_newstr(&l->u.s->str[intval(r)], 1, 0));
+        assign_str(l, s_newstr(&l->u.s->str[intval(r)], 1, 0));
         break;
     case TYPE_ARR: // TODO
     default:
-        assign_int(p, 0);
+        assign_int(l, 0);
         break;
     }
 }
@@ -188,27 +187,27 @@ static void put(val_t *v) {
 #define j16 (ip += (int16_t) ((ip[1] << 8) + ip[2]))
 
 // Conditional jumps (pop stack unconditionally)
-#define jc8(x)  (x ? j8  : (ip += 2)); sp--;
-#define jc16(x) (x ? j16 : (ip += 3)); sp--;
+#define jc8(x)  (x ? j8  : (ip += 2)); --sp;
+#define jc16(x) (x ? j16 : (ip += 3)); --sp;
 
 // Conditional jumps (pop stack if jump not taken)
-#define xjc8(x)  if (x) j8;  else {sp--; ip += 2;}
-#define xjc16(x) if (x) j16; else {sp--; ip += 3;}
+#define xjc8(x)  if (x) j8;  else {--sp; ip += 2;}
+#define xjc16(x) if (x) j16; else {--sp; ip += 3;}
 
 // Standard binary operations
 #define binop(x) \
-    z_##x(&sp[-2], deref(sp-2), deref(sp-1));\
-    sp--; \
-    ip++;
+    z_##x(sp[-2], sp[-1]); \
+    --sp; \
+    ++ip;
 
 // Unary operations
 #define unop(x) \
-    z_##x(&sp[-1], deref(sp-1)); \
-    ip++;
+    z_##x(sp[-1]); \
+    ++ip;
 
 // Pre-increment/decrement
 #define pre(x) { \
-    t = h_lookup(&globals, sp[-1].u.s); \
+    t = sp[-1]; \
     switch (t->type) { \
     case TYPE_INT: t->u.i += x; break; \
     case TYPE_FLT: t->u.f += x; break; \
@@ -216,12 +215,12 @@ static void put(val_t *v) {
         assign_int(t, x); \
         break; \
     } \
-    ip++; \
+    ++ip; \
 }
 
 // Post-increment/decrement
 #define post(x) { \
-    t = h_lookup(&globals, sp[-1].u.s); \
+    t = h_lookup(&globals, sp[-1]->u.s); \
     unop(num); \
     switch (t->type) { \
     case TYPE_INT: t->u.i += x; break; \
@@ -234,42 +233,54 @@ static void put(val_t *v) {
 
 // Compound assignment operations
 #define cbinop(x) { \
-    str_t *k = sp[-2].u.s; \
+    str_t *k = sp[-2]->u.s; \
     binop(x); \
-    h_insert(&globals, k, &sp[-1]); \
+    h_insert(&globals, k, sp[-1]); \
 }
 
 #define push(x) \
-    sp->type = x->type; \
-    sp->u    = x->u; \
-    sp++;
+    sp[0]->type = x->type; \
+    sp[0]->u    = x->u; \
+    ++sp;
 
 #define pushi(x) \
-    assign_int(sp, x); \
-    sp++;
+    assign_int(sp[0], x); \
+    ++sp;
 
-#define pushs(x) \
-    sp->type = TYPE_SYM; \
-    sp->u    = c->k.v[(x)]->u; \
-    sp++;
+#define pusha(x) \
+    sp[0] = h_lookup(&globals, c->k.v[(x)]->u.s); \
+    ++sp;
+
+#define pushv(x) \
+    t = h_lookup(&globals, c->k.v[(x)]->u.s); \
+    sp[0]->type = t->type; \
+    sp[0]->u = t->u; \
+    ++sp;
+
+// #define pushs(x) \
+//     sp->type = TYPE_SYM; \
+//     sp->u    = c->k.v[(x)]->u; \
+//     ++sp;
 
 int z_exec(code_t *c) {
     h_init(&globals);
-    val_t *sp = malloc(256 * sizeof(val_t));
+    val_t **sp = malloc(256 * sizeof(val_t *));
+    for (int i = 0; i < 256; i++)
+        sp[i] = NULL;
     val_t *t;
     uint8_t *ip = c->code;
     while (1) {
         switch (*ip) {
         case OP_JMP8:    j8;                 break;
         case OP_JMP16:   j16;                break;
-        case OP_JNZ8:    jc8(test(sp-1));    break;
-        case OP_JNZ16:   jc16(test(sp-1));   break;
-        case OP_JZ8:     jc8(!test(sp-1));   break;
-        case OP_JZ16:    jc16(!test(sp-1));  break;
-        case OP_XJNZ8:   xjc8(test(sp-1));   break;
-        case OP_XJNZ16:  xjc16(test(sp-1));  break;
-        case OP_XJZ8:    xjc8(!test(sp-1));  break;
-        case OP_XJZ16:   xjc16(!test(sp-1)); break;
+        case OP_JNZ8:    jc8(test(sp[-1]));    break;
+        case OP_JNZ16:   jc16(test(sp[-1]));   break;
+        case OP_JZ8:     jc8(!test(sp[-1]));   break;
+        case OP_JZ16:    jc16(!test(sp[-1]));  break;
+        case OP_XJNZ8:   xjc8(test(sp[-1]));   break;
+        case OP_XJNZ16:  xjc16(test(sp[-1]));  break;
+        case OP_XJZ8:    xjc8(!test(sp[-1]));  break;
+        case OP_XJZ16:   xjc16(!test(sp[-1])); break;
         case OP_TEST:    unop(test);         break;
         case OP_ADD:     binop(add);         break;
         case OP_SUB:     binop(sub);         break;
@@ -313,20 +324,21 @@ int z_exec(code_t *c) {
         case OP_SHRX:    cbinop(shr);        break;
         case OP_XORX:    cbinop(xor);        break;
         case OP_POP:
-            sp--;
-            ip++;
+            --sp;
+            ++ip;
             break;
         case OP_PUSH0:
             pushi(0);
-            ip++;
+            ++ip;
             break;
         case OP_PUSH1:
             pushi(1);
-            ip++;
+    printf("here\n");
+            ++ip;
             break;
         case OP_PUSH2:
             pushi(2);
-            ip++;
+            ++ip;
             break;
         case OP_PUSHI:
             pushi(ip[1]);
@@ -338,31 +350,47 @@ int z_exec(code_t *c) {
             break;
         case OP_PUSHK0:
             push(c->k.v[0]);
-            ip++;
+            ++ip;
             break;
         case OP_PUSHK1:
             push(c->k.v[1]);
-            ip++;
+            ++ip;
             break;
         case OP_PUSHK2:
             push(c->k.v[2]);
-            ip++;
+            ++ip;
             break;
         case OP_PUSHA:
-            pushs(ip[1]);
+            pusha(ip[1]);
             ip += 2;
             break;
         case OP_PUSHA0:
-            pushs(0);
-            ip++;
+            pusha(0);
+            ++ip;
             break;
         case OP_PUSHA1:
-            pushs(1);
-            ip++;
+            pusha(1);
+            ++ip;
             break;
         case OP_PUSHA2:
-            pushs(2);
-            ip++;
+            pusha(2);
+            ++ip;
+            break;
+        case OP_PUSHV:
+            pushv(ip[1]);
+            ip += 2;
+            break;
+        case OP_PUSHV0:
+            pushv(0);
+            ++ip;
+            break;
+        case OP_PUSHV1:
+            pushv(1);
+            ++ip;
+            break;
+        case OP_PUSHV2:
+            pushv(2);
+            ++ip;
             break;
         case OP_RET:
             exit(0);
@@ -372,16 +400,18 @@ int z_exec(code_t *c) {
             binop(idx);
             break;
         case OP_SET:
-            if (!IS_SYM((sp-2)))
-                err("Attempt to assign to constant value");
-            h_insert(&globals, sp[-2].u.s, deref(sp-1));
-            sp[-2] = *deref(sp-1);
-            sp--;
-            ip++;
+            // if (!IS_SYM((sp[-2])))
+            //     err("Attempt to assign to constant value");
+            // h_insert(&globals, sp[-2]->u.s, deref(sp[-1]));
+            sp[-2]->type = sp[-1]->type;
+            sp[-2]->u = sp[-1]->u;
+            --sp;
+            ++ip;
             break;
         case OP_PRINT:
-            put(deref(--sp));
-            ip++;
+            put(sp[-1]);
+            --sp;
+            ++ip;
             break;
         case OP_EXIT:
             exit(0);
--- src/vm.h
+++ src/vm.h
@@ -6,31 +6,31 @@
 #include "types.h"
 
 // VM operations exposed for (future) codegen optimizations
-void z_add(val_t *, val_t *, val_t *);
-void z_sub(val_t *, val_t *, val_t *);
-void z_mul(val_t *, val_t *, val_t *);
-void z_div(val_t *, val_t *, val_t *);
-void z_mod(val_t *, val_t *, val_t *);
-void z_pow(val_t *, val_t *, val_t *);
-void z_and(val_t *, val_t *, val_t *);
-void z_or(val_t *, val_t *, val_t *);
-void z_xor(val_t *, val_t *, val_t *);
-void z_shl(val_t *, val_t *, val_t *);
-void z_shr(val_t *, val_t *, val_t *);
-void z_num(val_t *, val_t *);
-void z_neg(val_t *, val_t *);
-void z_not(val_t *, val_t *);
-void z_eq(val_t *, val_t *, val_t *);
-void z_ne(val_t *, val_t *, val_t *);
-void z_gt(val_t *, val_t *, val_t *);
-void z_ge(val_t *, val_t *, val_t *);
-void z_lt(val_t *, val_t *, val_t *);
-void z_le(val_t *, val_t *, val_t *);
-void z_lnot(val_t *, val_t *);
-void z_len(val_t *, val_t *);
-void z_test(val_t *, val_t *);
-void z_cat(val_t *, val_t *, val_t *);
-void z_idx(val_t *, val_t *, val_t *);
+void z_add(val_t *, val_t *);
+void z_sub(val_t *, val_t *);
+void z_mul(val_t *, val_t *);
+void z_div(val_t *, val_t *);
+void z_mod(val_t *, val_t *);
+void z_pow(val_t *, val_t *);
+void z_and(val_t *, val_t *);
+void z_or(val_t *, val_t *);
+void z_xor(val_t *, val_t *);
+void z_shl(val_t *, val_t *);
+void z_shr(val_t *, val_t *);
+void z_num(val_t *);
+void z_neg(val_t *);
+void z_not(val_t *);
+void z_eq(val_t *, val_t *);
+void z_ne(val_t *, val_t *);
+void z_gt(val_t *, val_t *);
+void z_ge(val_t *, val_t *);
+void z_lt(val_t *, val_t *);
+void z_le(val_t *, val_t *);
+void z_lnot(val_t *);
+void z_len(val_t *);
+void z_test(val_t *);
+void z_cat(val_t *, val_t *);
+void z_idx(val_t *, val_t *);
 
 int  z_exec(code_t *);
 
