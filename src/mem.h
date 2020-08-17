577395255deea0fb81d4eaf5da3a887db85d4069
Darryl Abbate <rootbeersoup@gmail.com>
2020-08-17 17:02:14 -0500

    Add size parameter to m_growarray()

--- src/mem.h
+++ src/mem.h
@@ -3,10 +3,10 @@
 
 #include <stdlib.h>
 
-#define m_growarray(a, n, cap) \
+#define m_growarray(a, n, cap, sz) \
     if (cap <= n) { \
         cap = cap < 8 ? 8 : cap * 2; \
-        a = realloc(a, sizeof(a) * cap); \
+        a = realloc(a, sizeof(sz) * cap); \
     }
 
 #define m_freestr(s) \
