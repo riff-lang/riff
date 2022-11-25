2c23a7ad10eeec158b1731874ed903406cd58622
Darryl Abbate <rootbeersoup@gmail.com>
2022-11-25 00:29:08 -0800

    Make `riff_buf` a header-only utility

--- src/buf.h
+++ src/buf.h
@@ -2,6 +2,7 @@
 #define BUF_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 typedef struct {
     char   *buf;
@@ -9,10 +10,34 @@ typedef struct {
     size_t  cap;
 } riff_buf;
 
-void riff_buf_init_size(riff_buf *, size_t);
-void riff_buf_init(riff_buf *);
-void riff_buf_grow(riff_buf *);
-void riff_buf_free(riff_buf *);
-void riff_buf_resize(riff_buf *, size_t);
+static inline void riff_buf_init_size(riff_buf *b, size_t sz) {
+    b->buf = malloc(sz * sizeof (char));
+    b->n   = 0;
+    b->cap = sz;
+}
+
+static inline void riff_buf_init(riff_buf *b) {
+    riff_buf_init_size(b, 8);
+}
+
+static inline void riff_buf_free(riff_buf *b) {
+    free(b->buf);
+    b->n   = 0;
+    b->cap = 0;
+}
+
+static inline void riff_buf_grow(riff_buf *b) {
+    if (b->cap <= b->n) {
+        b->cap *= 2;
+        b->buf = realloc(b->buf, b->cap * sizeof (char));
+    }
+}
+
+static inline void riff_buf_resize(riff_buf *b, size_t sz) {
+    if (b->cap < b->n) {
+        b->buf = realloc(b->buf, sz * sizeof (char));
+        b->cap = sz;
+    }
+}
 
 #endif
