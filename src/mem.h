8fd8e4383f17fb3a229113628c75f1dc2979bad3
Darryl Abbate <rootbeersoup@gmail.com>
2022-12-27 21:53:24 -0800

    Remove unused macro

--- src/mem.h
+++ src/mem.h
@@ -11,12 +11,4 @@
         a = realloc(a, sizeof(sz) * cap); \
     }
 
-// Resizes a given buffer to the specified size ONLY IF the buffer
-// does not have enough space
-#define m_resizebuffer(b, n, cap, sz) \
-    if (cap < n) { \
-        cap = n; \
-        b = realloc(b, sizeof(sz) * cap); \
-    }
-
 #endif
